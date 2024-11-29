3b4316a725e0da1f2d9928b8926870ba032923be
Arnaud-Goguelin <arnaud.goguelin@gmail.com>
2024-11-29 12:26:41 +0100

    CI/CD: update settings for dynamic secret key and allowed hosts, and comment out static port binding in compose.yaml because it might be managed by Render

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,12 +10,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    default="fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 PORT = int(os.getenv("PORT", 8000))
 
 
